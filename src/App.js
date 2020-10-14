321931896203ec00586df843f844a7b6334e38e2
Yasoob Khalid <hi@yasoob.me>
2020-10-14 01:52:47 -0400

    added basic auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import {login, authFetch, useAuth, logout} from "./auth"
 
 export default function App() {
   return (
@@ -50,9 +51,87 @@ function Home() {
 }
 
 function Login() {
-  return <h2>Login</h2>;
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [logged] = useAuth();
+
+  const onSubmitClick = (e)=>{
+    e.preventDefault()
+    console.log("You pressed login")
+    let opts = {
+      'username': username,
+      'password': password
+    }
+    console.log(opts)
+    fetch('/api/login', {
+      method: 'post',
+      body: JSON.stringify(opts)
+    }).then(r => r.json())
+      .then(token => {
+        if (token.access_token){
+          login(token)
+          console.log(token)          
+        }
+        else {
+          console.log("Please type in correct username/password")
+        }
+      })
+  }
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {!logged? <form action="#">
+        <div>
+          <input type="text" 
+            placeholder="Username" 
+            onChange={handleUsernameChange}
+            value={username} 
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={handlePasswordChange}
+            value={password}
+          />
+        </div>
+        <button onClick={onSubmitClick} type="submit">
+          Login Now
+        </button>
+      </form>
+      : <button onClick={() => logout()}>Logout</button>}
+    </div>
+  )
 }
 
 function Secret() {
-  return <h2>Secret</h2>;
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    authFetch("/api/protected").then(response => {
+      if (response.status === 401){
+        setMessage("Sorry you aren't authorized!")
+        return null
+      }
+      return response.json()
+    }).then(response => {
+      if (response && response.message){
+        setMessage(response.message)
+      }
+    })
+  }, [])
+  return (
+    <h2>Secret: {message}</h2>
+  )
 }
